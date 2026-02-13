5e28f4b384791759927dff0f951e73e54eb1b130
Emanuel Minetti <e.minetti@posteo.de>
2026-02-13 01:20:23 +0100

    Ran first test and made it pass.

--- end2end/tests/example.spec.ts
+++ end2end/tests/example.spec.ts
@@ -5,5 +5,5 @@ test("homepage has title and links to intro page", async ({ page }) => {
 
   await expect(page).toHaveTitle("Welcome to Leptos");
 
-  await expect(page.locator("h1")).toHaveText("Welcome to Leptos!");
+  await expect(page.locator("h1")).toHaveText("Login");
 });
