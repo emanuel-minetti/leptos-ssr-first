b8a2f82a287b214039c863354bfe0604cf46ed74
Emanuel Minetti <e.minetti@posteo.de>
2026-05-02 23:21:07 +0200

    Made tests working again with an again increased timeout.

--- end2end/tests/i18n/user_lang.spec.ts
+++ end2end/tests/i18n/user_lang.spec.ts
@@ -71,7 +71,7 @@ test.describe("browser lang is german", async () => {
             await page.waitForURL("/");
             await loginPage.setLang(germanEnglish);
             await expect(page.getByRole('heading')).toHaveText(englishHomeTitle);
-            await page.waitForTimeout(1500);
+            await page.waitForTimeout(1900);
             expect(await dbHelper.getUserLang(username)).toBe("en");
             await browser.newContext();
             await page.evaluate(() => localStorage.removeItem('lang'));
